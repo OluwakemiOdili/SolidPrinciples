9b184a0866decd36add350c2e732129649b827d0
Support <Support@WEMA-TRAINING-S>
2022-08-11 18:59:11 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿namespace SolidPrinciples
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+          
+            var jewelryPriceList = new List<Sales>()
+            {
+                new NormalPrice(){CostOfData=100,CostOfGoods=100,TransportFare=2000,rate=0.2d},
+                new NormalPrice(){CostOfData=100,CostOfGoods=100,TransportFare=2000,rate=0.2d},
+                new NormalPrice(){CostOfData=100,CostOfGoods=100,TransportFare=2000,rate=0.2d},
+            };
+            var clothings = new List<Sales>
+            {
+                new BlackFriday(){CostOfGoods=2000,CostOfData=4000, TransportFare=2000, },
+                new BlackFriday(){CostOfGoods=2000,CostOfData=4000, TransportFare=2000, },
+                new BlackFriday(){CostOfGoods=2000,CostOfData=4000, TransportFare=2000, },
+            };
+            var tot = new TotalPrice();
+            Console.WriteLine(tot.TotalPriceExpected(jewelryPriceList));
+            Console.WriteLine(tot.TotalPriceExpected(clothings));
+            
+        }
+    }
+} 
---
+++ sales.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SolidPrinciples
+{
+    public abstract class Sales
+    {
+        public double CostOfGoods { get; set; }
+        public double TransportFare { get; set; }   
+        public double CostOfData { get; set; }
+        public abstract double PriceOfGoods();
+    }
+
+    public class TotalPrice
+    {
+        public double TotalPriceExpected(List<Sales> sales)
+        {
+            double amount=0;
+            try
+            {
+                foreach (var price in sales)
+                {
+                    amount += price.PriceOfGoods();
+                }
+                return amount;
+            }
+            catch (Exception err)
+            {
+
+                return 0;
+            }
+        }
+    }
+
+    public class NormalPrice:Sales
+    {
+        public double rate { get; set; }
+        public override double PriceOfGoods ()
+
+        {// this is the cost of sales
+            double price = ((CostOfGoods + TransportFare + CostOfData) * rate) + (CostOfGoods + TransportFare + CostOfData);
+            return price;
+           
+        } 
+     }
+
+    public class BlackFriday: Sales
+    {
+        // actual cost of goods/product to prevent funds loss after a long period of time 
+        public override double PriceOfGoods ()
+        {
+            double price = CostOfGoods + TransportFare + CostOfData;
+            return price;
+        }
+    }    
+
+}
